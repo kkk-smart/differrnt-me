ec9dd28b8268275d15ffb0da1c05f5772cf42b1d
KKK Smart <78399619+kkk-smart@users.noreply.github.com>
2021-06-23 11:45:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function first() {
+    window.location.href = "first.html";
+}
+
+function second() {
+    window.location.href = "second.html";
+}
+
+function th() {
+    window.location.href = "th.html";
+}
+
+function fou() {
+    window.location.href = "fou.html";
+}
